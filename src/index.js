f4787dc1c58e9d40de1305236f5eb89df20b2b63
wb-mwb403042@alibaba-inc.com <wb-mwb403042@alibaba-inc.com>
2021-07-14 16:23:09 +0800

    feat: 继承示例

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-class Person {
-  constructor(name) {
+// 父类
+class People {
+  constructor(name, age) {
     this.name = name;
+    this.age = age;
   }
-  getName() {
-    return this.name;
+  eat() {
+    alert(`${this.name} eat something`)
+  }
+  speak() {
+    alert(`My name is ${this.name} age ${this.age}`)
   }
 }
 
-const p = new Person('mwb');
-alert(p.getName());
+
+// 子类继承父类
+class Student extends People {
+  constructor(name, age, number) {
+    super(name, age);
+    this.number = number;
+  }
+  study() {
+    alert(`${this.name} study`)
+  }
+}
+
+const mwb = new Student('mwb', 27, 12);
+mwb.study();
+mwb.eat();
