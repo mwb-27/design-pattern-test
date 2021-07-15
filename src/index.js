cb1fd3bef7182e0bd1f2ac5ceea067b38a71af31
wb-mwb403042@alibaba-inc.com <wb-mwb403042@alibaba-inc.com>
2021-07-15 10:43:24 +0800

    ci: commitlint和prettier添加

--- src/index.js
+++ src/index.js
@@ -5,13 +5,12 @@ class People {
     this.age = age;
   }
   eat() {
-    alert(`${this.name} eat something`)
+    alert(`${this.name} eat something`);
   }
   speak() {
-    alert(`My name is ${this.name} age ${this.age}`)
+    alert(`My name is ${this.name} age ${this.age}`);
   }
 }
-
 
 // 子类继承父类
 class Student extends People {
@@ -20,10 +19,10 @@ class Student extends People {
     this.number = number;
   }
   study() {
-    alert(`${this.name} study`)
+    alert(`${this.name} study`);
   }
 }
 
-const mwb = new Student('mwb', 27, 12);
+const mwb = new Student("mwb", 27, 12);
 mwb.study();
 mwb.eat();
--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,27 @@
 
 // 父类
 class People {
-  name: string
-  age: number
-  private weight: number
-  protected father: string
-  constructor(name?: string, age?: number, weight?: number) {
+  name: string;
+  age: number;
+  private weight: number;
+  protected father: string;
+  constructor(name: string, age: number, weight: number) {
     this.name = name;
     this.age = age;
     this.weight = weight;
   }
   eat() {
-    alert(`${this.name} eat something`)
+    alert(`${this.name} eat something`);
   }
   speak() {
-    alert(`My name is ${this.name} age ${this.age}`)
+    alert(`My name is ${this.name} age ${this.age}`);
   }
 }
 
-
 // 子类继承父类
 class Student extends People {
-  number: number
-  private teacher: string
+  number: number;
+  private teacher: string;
   constructor(name: string, age: number, number: number, weight?: number) {
     super(name, age, weight);
     this.number = number;
@@ -32,22 +31,9 @@ class Student extends People {
     alert(`${this.name} study`);
   }
   speak() {
-    alert(`my teacher is ${this.teacher}, father is ${this.father}`)
+    alert(`my teacher is ${this.teacher}, father is ${this.father}`);
   }
 }
 
-const mwb = new Student('mwb', 27, 12);
+const mwb = new Student("mwb", 27, 12);
 mwb.speak();
-
-
-class A extends People {
-  constructor(name: string) {
-    super(name);
-  }
-  speak() {
-    alert('I am A');
-  }
-}
-
-const a = new A('a');
-a.speak();
